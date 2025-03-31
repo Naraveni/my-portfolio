af72b857467633c5417b0c3020b63908481aa7cb
naraveni <harshaemployment@gmail.com>
2025-03-30 23:06:58 -0400

    Deploy Commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ['a1a5-198-254-26-2.ngrok-free.app']
-  },
   plugins: [react(),
     tailwindcss(),
   ],
+  base: "/my-portfolio/",
 })
